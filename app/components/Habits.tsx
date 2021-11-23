823a346e2d74a49d2f39bf9a470997a312603d95
YukiH <yukikohagino@gmail.com>
2021-11-22 21:10:11 -0500

    Started making habits and date UI and logic

--- app/components/Habits.tsx
+++ app/components/Habits.tsx
@@ -1,13 +1,42 @@
 import tw, { styled, TwStyle, css } from "twin.macro";
 
 const HabitsContainer = styled.div`
-  ${tw`bg-blue flex justify-center content-center`}
+  ${tw`bg-yellow text-white flex justify-center content-center`}
+`;
+const HabitContainer = styled.div`
+  ${tw`text-blue flex justify-center content-center`}
 `;
 
 interface HighLightProps {
   containerStyles: TwStyle;
 }
 
+const habits = [
+  {
+    title: "Yoga",
+    _id: 1,
+    status: "ACTIVE"
+  }
+];
+
+const dates = [
+  {
+    activity: [
+      {
+        date: "11/11/2021",
+        _id: 2,
+        is_done: true
+      }
+    ]
+  }
+];
+
 export const Habits: React.FC<HighLightProps> = props => {
-  return <HabitsContainer>HABITS</HabitsContainer>;
+  return (
+    <HabitsContainer>
+      {habits.map(habit => (
+        <div>{habit.title}</div>
+      ))}
+    </HabitsContainer>
+  );
 };
