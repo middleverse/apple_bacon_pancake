e7f7c055bfb783ef63fef154961503e4f45f6baa
YukiH <yuki@chamino.com>
2021-11-27 22:39:55 -0500

    habits UIcontinues

--- app/components/Habits.tsx
+++ app/components/Habits.tsx
@@ -1,26 +1,30 @@
 import tw, { styled, TwStyle, css } from "twin.macro";
 
 const HabitsContainer = styled.div`
-  ${tw`bg-yellow text-white flex justify-center content-center`}
+  ${tw`bg-yellow text-blue flex justify-center content-center`}
 `;
 const HabitContainer = styled.div`
-  ${tw`text-blue flex justify-center content-center`}
+  ${tw`p-1.5 m-4 relative`}
 `;
 
 const HabitTitleContainer = styled.div`
-  ${tw`h-20 relative`}
+  ${tw`h-24 relative`}
 `;
 
 const HabitTitle = styled.div`
-  ${tw`text-blue transform rotate--45 absolute font-bold w-full pt-14`}
+  ${tw`transform rotate--45 absolute font-bold w-full pt-20`}
 `;
 
 const DoneHabit = styled.div`
-  ${tw`bg-red rounded-xl p-1.5 m-4`}
+  ${tw`bg-red rounded-xl p-1.5 absolute`}
 `;
 
 const DefaultHabit = styled.div`
-  ${tw`bg-grey rounded-xl p-1.5 m-4`}
+  ${tw`bg-grey rounded-xl p-1.5 absolute`}
+`;
+
+const DatesContainer = styled.div`
+  ${tw`mt-28 font-bold text-right mr-5 leading-7`}
 `;
 
 const habits = [
@@ -36,6 +40,16 @@ const habits = [
   },
   {
     title: "Read",
+    _id: 2,
+    status: "ACTIVE"
+  },
+  {
+    title: "Meditation",
+    _id: 2,
+    status: "ACTIVE"
+  },
+  {
+    title: "Run",
     _id: 2,
     status: "ACTIVE"
   }
@@ -57,7 +71,85 @@ const dates = {
         }
       ]
     },
-    "11/11/2021": {
+    "10/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "09/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "08/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "07/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "06/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "05/11/2021": {
+      date: "11/11/2021",
+      activity: [
+        {
+          activity_id: 1,
+          is_done: true
+        },
+        {
+          activity_id: 2,
+          is_done: false
+        }
+      ]
+    },
+    "04/11/2021": {
       date: "11/11/2021",
       activity: [
         {
@@ -73,30 +165,57 @@ const dates = {
   }
 };
 
-const getActivityArray = function (dates, habit_id) {
-  let habitActivity = [true, false, true, true, true];
-  return habitActivity;
+const getDatesArray = function (dates) {
+  let array = [];
+  for (const date in dates.dates) {
+    array.push(date);
+  }
+  console.log(array);
+  return array;
 };
 
-const array = [true, false, true, true, true];
+const arrays = [
+  [true, false, true, true, true, true, false, true, true, true],
+  [false, false, true, false, true, true, false, true, true, true],
+  [true, true, true, false, true, true, true, true, false, true],
+  [true, false, true, true, true, true, false, true, true, true],
+  [false, false, true, false, true, true, false, true, true, true]
+];
+
+const checkDone = function (habitIndex, itemIndex) {
+  return arrays[habitIndex][itemIndex];
+};
 
 export const Habits: React.FC = () => {
   return (
     <HabitsContainer>
-      {habits.map(habit => (
-        <div>
-          <HabitTitleContainer>
-            <HabitTitle>{habit.title}</HabitTitle>
-          </HabitTitleContainer>
-          {array.map((is_done, index) =>
-            is_done ? (
-              <DoneHabit key="{habit.title}-{index}"></DoneHabit>
-            ) : (
-              <DefaultHabit key="{habit.title}-{index}" />
-            )
-          )}
-        </div>
-      ))}
+      <DatesContainer>
+        {getDatesArray(dates).map((date, dateIndex) => (
+          <div>{date}</div>
+        ))}
+      </DatesContainer>
+      <HabitsContainer>
+        {habits.map((habit, habitIndex) => (
+          <div>
+            <HabitTitleContainer>
+              <HabitTitle>{habit.title}</HabitTitle>
+            </HabitTitleContainer>
+            {arrays[habitIndex].map((is_done, itemIndex) =>
+              is_done ? (
+                <HabitContainer key="{habit.title}-{index}">
+                  <DoneHabit
+                    css={[checkDone(habitIndex, itemIndex + 1) && tw`pb-8`]}
+                  ></DoneHabit>
+                </HabitContainer>
+              ) : (
+                <HabitContainer key="{habit.title}-{index}">
+                  <DefaultHabit />
+                </HabitContainer>
+              )
+            )}
+          </div>
+        ))}
+      </HabitsContainer>
     </HabitsContainer>
   );
 };
